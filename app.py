dc71c0a9b234aad95cbb90e486be789e2d4b5a3c
Trevor Beers <98654174+trevyyy@users.noreply.github.com>
2024-02-11 12:31:47 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Request, HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
